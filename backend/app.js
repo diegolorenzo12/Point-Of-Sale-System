11713ef7482f7a21bf92e658781fcb1851bdae2c
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2023-09-20 21:53:28 -0700

    added users scheme

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const User = require("./models/users");
+//const router = express.Router();
 const app = express();
-
 const port = 3000;
 
 const db = process.env.ATLAS_URI;
+mongoose.connect(db, { useNewUrlParser: true });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-mongoose.connect(db, { useNewUrlParser: true });
