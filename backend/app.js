4bee95d4b57eb757fe4049d54f66336de265222a
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2023-09-20 20:22:59 -0700

    db connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
+
 const port = 3000;
+
+const db = process.env.ATLAS_URI;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+mongoose.connect(db, { useNewUrlParser: true });
