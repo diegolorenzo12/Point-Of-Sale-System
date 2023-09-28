f66f90e4d82e7f6057516b120f9e8ffca683d472
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2023-09-27 23:30:46 -0700

    tabitems now uses the producs api

--- client/src/app/edit/page.tsx
+++ client/src/app/edit/page.tsx
@@ -1,9 +1,69 @@
 'use client'
-import React from 'react'
+import React, {useState} from 'react'
 import {Button, Input} from "@nextui-org/react";
+import axios from 'axios';
 
 
 export default function page() {
+  const [formData, setFormData] = useState({
+    name: '',
+    category: '',
+    description: '',
+    brand: '',
+    price: '',
+    stock: '',
+  });
+
+    // File input state
+  const [file, setFile] = useState(null);
+
+  const handleInputChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+  console.log(formData)
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+const handleSubmit = async () => {
+  try {
+    // Create a FormData object
+    const formDataObject = new FormData();
+
+    // Append the file
+    formDataObject.append('file', file);
+
+    // Append other form data fields
+    formDataObject.append('name', formData.name);
+    formDataObject.append('category', formData.category);
+    formDataObject.append('description', formData.description);
+    formDataObject.append('brand', formData.brand);
+    formDataObject.append('price', formData.price);
+    formDataObject.append('stock', formData.stock);
+
+    // Make a POST request using Axios with FormData
+    await axios.post('http://localhost:3001/api/products', formDataObject);
+
+    // Reset the form after successful submission
+    setFormData({
+      name: '',
+      category: '',
+      description: '',
+      brand: '',
+      price: '',
+      stock: '',
+    });
+    setFile(null);
+
+  } catch (error) {
+    // Handle errors here
+    console.error('Error:', error);
+  }
+};
+
+
   return (
     <div className='text-center mx-auto'>
       <h1 className='mb-7 font-bold'>Add New Product</h1>
@@ -14,6 +74,8 @@ export default function page() {
           type="text"
           label="name"
           labelPlacement="inside"
+          isRequired
+          onValueChange={(value)=>handleInputChange("name", value)}
         />
         <Input
           className='m-2 w-1/4'
@@ -21,6 +83,8 @@ export default function page() {
           type="text"
           label="category"
           labelPlacement="inside"
+          isRequired
+          onValueChange={(value)=>handleInputChange("category", value)}
         />
         <Input
           className='m-2 w-1/4'
@@ -28,6 +92,8 @@ export default function page() {
           type="text"
           label="description"
           labelPlacement="inside"
+          isRequired
+          onValueChange={(value)=>handleInputChange("description", value)}
         />
         <Input
           className='m-2 w-1/4'
@@ -35,6 +101,8 @@ export default function page() {
           type="text"
           label="Brand"
           labelPlacement="inside"
+          isRequired
+          onValueChange={(value)=>handleInputChange("brand", value)}
         />
         <Input
           className='m-2 w-1/4'
@@ -42,6 +110,8 @@ export default function page() {
           type="number"
           label="Price"
           labelPlacement="inside"
+          isRequired
+          onValueChange={(value)=>handleInputChange("price", value)}
         />
         <Input
           className='m-2 w-1/4'
@@ -49,8 +119,11 @@ export default function page() {
           type="number"
           label="stock"
           labelPlacement="inside"
+          isRequired
+          onValueChange={(value)=>handleInputChange("stock", value)}
         />
-        <Button color='primary'>Add</Button>
+        <input type="file" accept=".jpg, .png" name="file" onChange={handleFileChange}/>
+        <Button color='primary' onClick={handleSubmit}>Add</Button>
       </div>
     </div>
   )
--- client/src/app/sell/components/TabItem.tsx
+++ client/src/app/sell/components/TabItem.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Card, CardBody, CardFooter, button } from '@nextui-org/react'
 import Image, { StaticImageData } from 'next/image'
 import frutas from "../../assets/Fruit-HD-Wallpapers-03484.jpg"
 import masFrutas from "../../assets/GM-White-Page-214.jpg"
+import axios from 'axios';
 
 type CuentaItem = {
     name: string;
@@ -10,56 +11,41 @@ type CuentaItem = {
 };
 
 type ItemType = {
-    img: StaticImageData;
+    img: string;
     name: string;
     value: number;
 };
 
-
-//this values should be fetch from api
-const frutasDommy: ItemType[]=[
-    {
-        img:frutas,
-        name: "Fruta",
-        value: 10
-    },
-    {
-        img:masFrutas,
-        name: "Fruta2",
-        value: 15
-    },
-        {
-        img:masFrutas,
-        name: "Fruta2",
-        value: 15
-    },
-        {
-        img:masFrutas,
-        name: "Fruta2",
-        value: 15
-    },
-        {
-        img:masFrutas,
-        name: "Fruta2",
-        value: 15
-    },
-        {
-        img:masFrutas,
-        name: "Fruta2",
-        value: 15
-    },
-        {
-        img:masFrutas,
-        name: "Fruta2",
-        value: 15
-    }
-];
-
 export default function TabItem({cuenta, setCuenta, category}: {cuenta:CuentaItem[], setCuenta:React.Dispatch<React.SetStateAction<CuentaItem[]>>, category:String }) {
-    
+  
+  const [productData, setProductData] = useState<ItemType[]>([]);
+  useEffect(() => {
+    // Define your API endpoint URL
+    const apiUrl = `http://localhost:3001/api/products/?category=${category}`;
+
+    // Fetch data from the API using Axios
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        // Map the API response to match the ItemType structure
+        const mappedData: ItemType[] = response.data.map((item: any) => ({
+          img: item.imageUrl,
+          name: item.name,
+          value: item.price, // Assuming 'price' is the value you want to display
+        }));
+
+        // Update state with the mapped data
+        setProductData(mappedData);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [category]);
+
+  console.log(productData);
     return (
     <div className='flex flex-row flex-wrap'>
-        {frutasDommy.map((item, index) => (
+        {productData.map((item, index) => (
         <Card
           key={index}
           shadow="sm"
